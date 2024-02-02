53e6a043b52cecca5addfa2190f9285e9bd6b633
sracabanahashas <wkocsis@gmail.com>
2024-02-02 08:30:02 -0800

    Finish playRound() function using a lot of if statements

--- javascript.js
+++ javascript.js
@@ -15,7 +15,6 @@ function multiply(a, b) {
 }
 
 // Make the computer's play by randomly selecting "Rock", "Paper", or "Scissors"//
-
 function getComputerChoice() {
     let randomSelection = Math.random();
     console.log(randomSelection);
@@ -32,7 +31,7 @@ function getComputerChoice() {
 return computerSelection;
 }
 
-
+// Play a round by asking for player's choice, then determining winner based on computer's play//
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Rock, paper or scissors?");
     playerSelection = playerSelection.toUpperCase();
@@ -43,5 +42,21 @@ function playRound(playerSelection, computerSelection) {
 
     if ((playerSelection === "ROCK") && (computerSelection === "Scissors")) {
         console.log("You win! Rock beats Scissors!");
+    } else if ((playerSelection === "PAPER") && (computerSelection === "Scissors")) {
+        console.log("You lose! Scissors beat paper!");
+    } else if ((playerSelection === "SCISSORS") && (computerSelection === "Scissors")) {
+        console.log("A tie!");
+    } else if ((playerSelection === "ROCK") && (computerSelection === "Paper")) {
+        console.log("You lose! Paper beats rock!");
+    } else if ((playerSelection === "PAPER") && (computerSelection === "Paper")) {
+        console.log("A tie!");
+    } else if ((playerSelection === "SCISSORS") && (computerSelection === "Paper")) {
+        console.log("You win! Scissors beat paper!");
+    } else if ((playerSelection === "ROCK") && (computerSelection === "Rock")) {
+        console.log("A tie!");
+    } else if ((playerSelection === "PAPER") && (computerSelection === "Rock")) {
+        console.log("You win! Paper beats rock!");
+    } else if ((playerSelection === "SCISSORS") && (computerSelection === "Rock")) {
+        console.log("You lose! Rock beats scissors!");
     }
 }
