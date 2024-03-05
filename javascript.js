fd9bcf2d370c0cb4c61a4a7c216d82317410391d
sracabanahashas <wkocsis@gmail.com>
2024-03-05 09:03:28 -0800

    Create running score tally and winner announcement in DOM.

--- javascript.js
+++ javascript.js
@@ -22,47 +22,134 @@ function playRound(playerSelection, computerSelection, winner) {
     //playerSelection = playerSelection;
     //playerSelection = playerSelection.toUpperCase();
     console.log(playerSelection);
+    resultItem = document.createElement('ul');
+    resultItem.textContent = 'Player used: ' + playerSelection + '!';
+    console.log(resultItem);
+    result.appendChild(resultItem);
     
     computerSelection = getComputerChoice();
     console.log(computerSelection);
+    resultItem = document.createElement('ul');
+    resultItem.textContent = 'Computer used: ' + computerSelection + '!';
+    console.log(resultItem);
+    result.appendChild(resultItem);
 
-    //console.log(winner);
+    
 
     if ((playerSelection === "ROCK") && (computerSelection === "Scissors")) {
         winner = "Player";
         console.log(winner);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = winner + ' wins!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Player score: ' + ++playerScore;
+        result.appendChild(resultItem);
         return winner;
+
     } else if ((playerSelection === "PAPER") && (computerSelection === "Scissors")) {
         winner = "Computer";
         console.log(winner);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = winner + ' wins!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Computer score: ' + ++computerScore;
+        result.appendChild(resultItem);
+
         return winner;
+
     } else if ((playerSelection === "SCISSORS") && (computerSelection === "Scissors")) {
         winner = "None";
         console.log(winner);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'A tie!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Player score: ' + playerScore + '. ' + "Computer score: " + computerScore;
+        result.appendChild(resultItem);
+
         return winner;
     } else if ((playerSelection === "ROCK") && (computerSelection === "Paper")) {
         winner = "Computer";
         console.log(winner);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = winner + ' wins!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Computer score: ' + ++computerScore;
+        result.appendChild(resultItem);
+
         return winner;
+
     } else if ((playerSelection === "PAPER") && (computerSelection === "Paper")) {
         winner = "None";
         console.log(winner);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'A tie!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Player score: ' + playerScore + '. ' + "Computer score: " + computerScore;
+        result.appendChild(resultItem);
+
         return winner;
+
     } else if ((playerSelection === "SCISSORS") && (computerSelection === "Paper")) {
         winner = "Player";
         console.log(winner);
+        resultItem = document.createElement('ul');
+        resultItem.textContent = winner + ' wins!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Player score: ' + ++playerScore;
+        result.appendChild(resultItem);
         return winner;
+
     } else if ((playerSelection === "ROCK") && (computerSelection === "Rock")) {
         winner = "None";
         console.log(winner);
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'A tie!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Player score: ' + playerScore + '. ' + "Computer score: " + computerScore;
+        result.appendChild(resultItem);
         return winner;
+
     } else if ((playerSelection === "PAPER") && (computerSelection === "Rock")) {
         winner = "Player";
         console.log(winner);
+        resultItem = document.createElement('ul');
+        resultItem.textContent = winner + ' wins!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Player score: ' + ++playerScore;
+        result.appendChild(resultItem);
         return winner;
+
     } else if ((playerSelection === "SCISSORS") && (computerSelection === "Rock")) {
         winner = "Computer";
         console.log(winner);
+        resultItem = document.createElement('ul');
+        resultItem.textContent = winner + ' wins!';
+        result.appendChild(resultItem);
+
+        resultItem = document.createElement('ul');
+        resultItem.textContent = 'Computer score: ' + ++computerScore;
+        result.appendChild(resultItem);
         return winner;
     }
 }
@@ -70,6 +157,11 @@ function playRound(playerSelection, computerSelection, winner) {
 rockBtn = document.querySelector('#rock');
 paperBtn = document.querySelector('#paper');
 scissorsBtn = document.querySelector('#scissors');
+result = document.querySelector('#result');
+
+let roundWinner;
+let playerScore = 0;
+let computerScore = 0;
 
 rockBtn.addEventListener('click', () => {
     playRound('ROCK');
@@ -80,9 +172,22 @@ paperBtn.addEventListener('click', () => {
 });
 
 scissorsBtn.addEventListener('click', () => {
-    playRound('SCISSORS');
+    roundWinner = playRound('SCISSORS');
 });
 
+document.addEventListener('click', () => {
+    if (playerScore == 5) {
+        resultItem = document.createElement('ul');
+            resultItem.textContent = 'Player wins first to 5!';
+            result.appendChild(resultItem);
+    };
+    
+    if (computerScore == 5) {
+        resultItem = document.createElement('ul');
+            resultItem.textContent = 'Computer wins first to 5!';
+            result.appendChild(resultItem);
+    };
+})
 
 // Play 5 rounds of Rock Paper Scissors, keeping score and declaring a winner at the end
 /*function playGame() {
