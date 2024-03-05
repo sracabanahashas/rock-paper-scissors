bfae3de2acf6e096fcbfe677ac00ebc6c7409a32
sracabanahashas <wkocsis@gmail.com>
2024-03-05 09:04:47 -0800

    Delete unused code in javscript.js

--- javascript.js
+++ javascript.js
@@ -19,8 +19,6 @@ return computerSelection;
 
 // Play a round by asking for player's choice, then determining winner based on computer's play//
 function playRound(playerSelection, computerSelection, winner) {
-    //playerSelection = playerSelection;
-    //playerSelection = playerSelection.toUpperCase();
     console.log(playerSelection);
     resultItem = document.createElement('ul');
     resultItem.textContent = 'Player used: ' + playerSelection + '!';
@@ -188,70 +186,3 @@ document.addEventListener('click', () => {
             result.appendChild(resultItem);
     };
 })
-
-// Play 5 rounds of Rock Paper Scissors, keeping score and declaring a winner at the end
-/*function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let roundWinner = playRound();
-    console.log(roundWinner);
-
-    if (roundWinner === "Player") {
-        playerScore = ++playerScore;
-    } else if (roundWinner === "Computer") {
-        computerScore = ++computerScore;
-    }
-
-    console.log(playerScore, computerScore)
-
-    roundWinner = playRound();
-    console.log(roundWinner);
-
-    if (roundWinner === "Player") {
-        playerScore = ++playerScore;
-    } else if (roundWinner === "Computer") {
-        computerScore = ++computerScore;
-    }
-
-    console.log(playerScore, computerScore)
-
-    roundWinner = playRound();
-    console.log(roundWinner);
-
-    if (roundWinner === "Player") {
-        playerScore = ++playerScore;
-    } else if (roundWinner === "Computer") {
-        computerScore = ++computerScore;
-    }
-
-    console.log(playerScore, computerScore)
-
-    roundWinner = playRound();
-    console.log(roundWinner);
-
-    if (roundWinner === "Player") {
-        playerScore = ++playerScore;
-    } else if (roundWinner === "Computer") {
-        computerScore = ++computerScore;
-    }
-
-    console.log(playerScore, computerScore)
-
-    roundWinner = playRound();
-    console.log(roundWinner);
-
-    if (roundWinner === "Player") {
-        playerScore = ++playerScore;
-    } else if (roundWinner === "Computer") {
-        computerScore = ++computerScore;
-    }
-
-    console.log(playerScore, computerScore)
-
-    if (playerScore > computerScore) {
-        alert("You win best of 5!")
-    } else if (playerScore < computerScore) {
-        alert("Computer wins best of 5!")
-    } else {alert("A 5-round tie!")}
-}*/
